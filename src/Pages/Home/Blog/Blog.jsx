4d7db2c0b0d50767815de704f6194fcff32b77fa
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-01-29 02:08:08 +0600

    implement framer motion on blog section

--- src/Pages/Home/Blog/Blog.jsx
+++ src/Pages/Home/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import * as motion from "motion/react-client"
 import React, { useEffect, useState } from 'react'
 
 export const Blog = () => {
@@ -18,11 +19,12 @@ export const Blog = () => {
            <section className='grid grid-cols-1 md:grid-cols-3 gap-8'>
             {
                 blogs.map(blog=><div key={blog.id} className="card card-compact bg-base-100 rounded-none  shadow-xl">
-                    <figure>
+                    <motion.div whileHover={{ scale: 0.95 }}
+            whileTap={{ scale: .001 }}>
                       <img className='h-[400px] object-cover'
                         src={blog.image}
                         alt="blog" />
-                    </figure>
+                    </motion.div>
                     <div className="card-body space-y-2">
                       <h2 className="card-title">{blog.title}</h2>
                       <p className='line-clamp-2 opacity-80'>{blog.description}</p>
